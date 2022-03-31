7feaf7b898bab2249f4c279a04ce0f6eb255cd48
tristanhannibal <tristanhannibal@gmail.com>
2022-03-30 19:59:34 -0500

    Add params to about

--- SolarSize/resources/js/app.js
+++ SolarSize/resources/js/app.js
@@ -17,15 +17,15 @@ import Vue from 'vue';
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
-    // Optionally install the BootstrapVue icon components plugin
+// Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
-    /**
-     * The following block of code may be used to automatically register your
-     * Vue components. It will recursively scan this directory for the Vue
-     * components and automatically register them with their "basename".
-     *
-     * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
-     */
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
