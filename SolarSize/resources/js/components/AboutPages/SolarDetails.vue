8f6ef9bea8cf0360674a147ea1bec6da5e74d5d7
KadenGoski <55845863+KadenGoski@users.noreply.github.com>
2022-04-06 23:08:03 -0600

    Update About Page

--- SolarSize/resources/js/components/AboutPages/SolarDetails.vue
+++ SolarSize/resources/js/components/AboutPages/SolarDetails.vue
@@ -6,10 +6,36 @@
         <template v-slot:header>
           <h3>Solar Model</h3>
         </template>
-        <p>Our solar estimation model uses “all sky” global irradiance values (GHI) from the NASA Power API along with dew points, temperatures, pressure at surface, and solar zenith angles to calculate the direct normal irradiance and direct horizontal irradiance on a solar panel at the given location. This is achieved through converted global horizontal irradiance values from the API call to direct horizontal and direct normal (DNI and DHI) values using equations and the DIRINT model (will add more information and sources later).
+        <p>Our solar estimation model uses “all sky” global irradiance values (GHI) from the NASA POWER API along with dew points, temperatures, pressure at surface, and solar zenith angles to calculate the direct normal irradiance and direct horizontal irradiance on a solar panel at the given location. This is achieved through converted global horizontal irradiance values from the API call to direct normal irradiance and diffuse horizontal irradiance (DNI and DHI) values using equations and the DIRINT model.
 
-        DNI and DHI values are then used in conjunction with specifications of the solar panels given by user inputs (panel efficiency, panel location and direction, overall system efficiency, number of panels) to calculate the expected output in KWH in hourly intervals for the desired data range. As of March 2022, the NASA API has data available from mid 2021 and earlier.
+        DNI and DHI values are then used in conjunction with specifications of the solar panels given by user inputs (panel efficiency, panel location and direction, overall system efficiency, number of panels) to calculate the expected output in kWh in hourly intervals for the desired data range. As of March 2022, the NASA API has data available from fall 2021 and earlier.
         </p>
+        <template v-slot:footer> </template>
+      </ROIText>
+      <ROIText>
+        <template v-slot:header>
+          <h3>What is GHI, DHI, and DNI?</h3>
+        </template>
+        <p> GHI, DHI, and DNI are solar irradiance components. GHI is global horizontal irradiance and is equivalent to the global solar irradiance on a horizontal surface.
+        </p>
+        <div
+            v-katex="'\\small Global\\ Horizontal\\ Irradiance\\ = \\large DHI \\small + \\large DNI \\small * \\cos (Panel Angle)'"></div>   
+            <p> 
+            DHI is diffuse horizontal irradiance and is the light that reflects/diffuses off of clouds, dust, the ground and other surfaces before hitting the panel. 
+            </p>
+            <div
+            v-katex="'\\small Diffuse\\ Horizontal\\ Irradiance = \\large GHI \\small -  \\large DNI\\ * \\cos (PanelAngle)'"></div>
+            <p>
+             The equivalent diffuse radiation that hits an angled panel is:
+            </p>
+            <div
+            v-katex="'\\small Panel\\ Angled\\ Diffuse\\ Irradiance = \\large DHI \\small * \\frac{180 - Panel Angle}{180}'"></div>
+          <p>
+          DNI is direct normal irradiance and is the light that directly hits the panel, perpendicular to its angle. In our model, it is estimated from GHI by using the DIRINT model from <a href="https://pvlib-python.readthedocs.io/en/latest/reference/generated/pvlib.irradiance.dirint.html?highlight=DIRINT#pvlib.irradiance.dirint" target="_blank" rel="noopener noreferrer">pvlib</a> which performs complex time derivative math estimations to get a value (more information on this can be found <a href="https://www.semanticscholar.org/paper/Dynamic-global-to-direct-irradiance-conversion-Ineichen-Perez/0da5f4e6bdb0f42eb10d45607cce1df13e08961a" target="_blank" rel="noopener noreferrer">here</a>).
+          To get the value of DNI on a tilted panel, we must use the complex equation below from <a href="https://www.pveducation.org/pvcdrom/properties-of-sunlight/making-use-of-tmy-data" target="_blank" rel="noopener noreferrer">pveducation</a>. The Solar Declination Angle and HRA (Solar Hour Angle) will be explained in the Solar Time section on this page.
+          </p>
+          <div
+            v-katex="'\\small Panel\\ Direct\\ Normal\\ Irradiance = \\large DNI \\small (\\sin (δ) \\sin (φ) \\cos (β)\\ -\\ \\sin (δ) \\cos (φ) \\sin (β) \\cos (ψ)\\ + \\cos (δ) \\cos (φ) \\cos (β) \\cos (HRA)\\ + \\cos (δ) \\sin (φ) \\sin (β) \\cos (ψ) \\cos (HRA)\\ + \\cos (δ) \\sin (ψ) \\sin (HRA) \\sin (β)) \\newline \\space \\newline  where\\ \\newline \\space \\newline δ\\ is\\ the\\ Declination\\ Angle\\ \\newline  φ\\ is\\ the\\ Latitude\\ \\newline  β\\ is\\ the\\ panel\\ tilt\\ \\newline  ψ\\ is\\ the\\ panel\\ azimuth\\ angle\\ (panel\\ direction/orientation\\ angle)\\ \\newline HRA\\ is\\ solar\\ hour\\ angle\\ '"></div>
         <template v-slot:footer> </template>
       </ROIText>
     </div>
