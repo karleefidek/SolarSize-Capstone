7242daefe774ea1dc8ba9f9f16afc3fa7a154a39
karleefidek <55856535+karleefidek@users.noreply.github.com>
2022-08-17 20:50:21 -0600

    Delete SolarSize directory

--- SolarSize/resources/js/main.js
+++
@@ -1,7 +0,0 @@
-// import Vue from 'vue'
-// import App from './components/App.vue'
-// Vue.config.productionTip = false;
-// export const bus = new Vue();
-// new Vue({
-//   render: h => h(App),
-// }).$mount('#app')
